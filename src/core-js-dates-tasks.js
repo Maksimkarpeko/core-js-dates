c3e329d61a333a1d3e50140c4b8271514e30ec2f
Maksim <maksim_karpe2ko@mail.ru>
2026-01-04 14:21:30 +0300

    feat:completed getNextFridayThe13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -213,8 +213,16 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + 1);
+
+  while (true) {
+    if (newDate.getDate() === 13 && newDate.getDay() === 5) {
+      return new Date(newDate);
+    }
+    newDate.setDate(newDate.getDate() + 1);
+  }
 }
 
 /**
